0929a71280fac02e5cc027c0d3c825f4e9bfdafa
Kamil Maksymowicz <maxio090124@interia.pl>
2020-06-13 14:23:33 +0200

    odkomentowanie

--- main.py
+++ main.py
@@ -82,26 +82,26 @@ class Game(object):
         try:
             self.main_menu()
             if self.main_menu()==0:
-            #     lvl1=self.lvl_creator(1,3,3,-1,2)
-            # if lvl1==0:
-            #     lvl2=self.lvl_creator(2,4,3,-2,1)
-            # if lvl2==0:
-            #     lvl3=self.lvl_creator(3,8,3,-2,1)
-            # if lvl3 == 0:
-            #     lvl4 = self.lvl_creator(4, 10, 3, -2, 2)
-            # if lvl4 == 0:
-            #     lvl5 = self.lvl_creator(5, 15, 3, -3, 2)
-            # if lvl5 == 0:
-            #     lvl6 = self.lvl_creator(6, 20, 3, -2, 3)
-            # if lvl6 == 0:
-            #     lvl7 = self.lvl_creator(7, 25, 3, -3, 3)
-            # if lvl7 == 0:
-            #     lvl8 = self.lvl_creator(8, 27, 3, -3, 4)
-            # if lvl8 == 0:
-            #     lvl9 = self.lvl_creator(9, 29, 3, -4, 4)
-            # if lvl9 == 0:
-            #      lvl10= self.lvl_creator(10,29,6,-7,5)
-            #             # if lvl10 == 0:
+                lvl1=self.lvl_creator(1,3,3,-1,2)
+            if lvl1==0:
+                lvl2=self.lvl_creator(2,4,3,-2,1)
+            if lvl2==0:
+                lvl3=self.lvl_creator(3,8,3,-2,1)
+            if lvl3 == 0:
+                lvl4 = self.lvl_creator(4, 10, 3, -2, 2)
+            if lvl4 == 0:
+                lvl5 = self.lvl_creator(5, 15, 3, -3, 2)
+            if lvl5 == 0:
+                lvl6 = self.lvl_creator(6, 20, 3, -2, 3)
+            if lvl6 == 0:
+                lvl7 = self.lvl_creator(7, 25, 3, -3, 3)
+            if lvl7 == 0:
+                lvl8 = self.lvl_creator(8, 27, 3, -3, 4)
+            if lvl8 == 0:
+                lvl9 = self.lvl_creator(9, 29, 3, -4, 4)
+            if lvl9 == 0:
+                 lvl10= self.lvl_creator(10,29,6,-7,5)
+            if lvl10 == 0:
                 print("finish")
                 return None
         except TypeError:
@@ -223,7 +223,7 @@ class Game(object):
         except TypeError:
             raise Exception("Type Error")
         except Exception:
-            raise Exception("Error")
+            raise Exception("Error - nextlevelbutton")
 
     ########################MAIN MENU###############
     def main_menu(self):
